3c0c369bf4683ee83dcb69ab24aa769245e65449
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-06-02 08:58:19 +0200

    Esercizio completato

--- src/CiclatoreInteri.java
+++ src/CiclatoreInteri.java
@@ -1,5 +1,30 @@
 public class CiclatoreInteri {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        private int[] elencoInteri;
+        private int indiceAttuale = 0;
+
+    // Costruttore 
+        public CiclatoreInteri(int[] elenco) {
+            // Inizializza l'elenco e l'indice corrente
+            this.elencoInteri = elenco;
+            if (this.elencoInteri.length > 0) {
+                indiceAttuale = 0;
+            }
+        }
+
+    //Metodi 
+    // Prende l'elemento successivo nell'elenco
+        public int getElementoSuccessivo() {
+            int elemento = elencoInteri[indiceAttuale];
+            this.indiceAttuale++;
+            return elemento;
+        }
+
+    // Controllo se ci sono ancora elementi nell'elenco
+        boolean hasAncoraElementi() {
+            if (elencoInteri.length == 0) {
+                return false;
+            }
+            return this.indiceAttuale < this.elencoInteri.length;
+
+        }
 }
